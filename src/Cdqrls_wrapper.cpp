aeb7ee8ce5751619a057a2831f17f40dd11d0022
boennecd <boennecd@gmail.com>
2016-09-15 09:06:21 +0200

    Added code to match the glm.fit in cpp. It calls the same C code for the QR solving method of the IWLS

---
+++ src/Cdqrls_wrapper.cpp
@@ -0,0 +1,210 @@
+// [[Rcpp::depends("RcppArmadillo")]]
+// [[Rcpp::plugins(cpp11)]]
+#include <RcppArmadillo.h>
+#include <armadillo>
+
+extern "C" {
+  #include <R_ext/Constants.h> // for DOUBLE_EPS
+  #include "stats/statsR.h"
+  SEXP Cdqrls(SEXP x, SEXP y, SEXP tol, SEXP chk);
+}
+
+class Cdqrls_res {
+public:
+  Cdqrls_res(Rcpp::List res):
+  coefficients(Rcpp::as<arma::vec>(res["coefficients"])),
+  pivot(Rcpp::as<arma::uvec>(res["pivot"]) - 1)
+  {}
+
+  const arma::vec coefficients;
+  const arma::uvec pivot; // this differs from initial values by being zero indexed
+};
+
+Cdqrls_res Cdqrls_wrapper(const arma::mat &X, const arma::vec &y,
+                          const double eps, const bool check){
+  Rcpp::List res = Rcpp::as<Rcpp::List>(
+    Cdqrls(Rcpp::wrap(X), Rcpp::wrap(y), Rcpp::wrap(eps), Rcpp::wrap(check)));
+
+  return(Cdqrls_res(res));
+}
+
+
+
+
+
+
+class dist_family {
+public:
+  virtual double link_func(const double&) const = 0;
+  virtual double link_func_inv(const double&) const = 0;
+  virtual double variance(const double&) const = 0;
+  virtual double d_mu_d_eta(const double&) const = 0; // d mu / d eta
+  virtual double dev_resids(const double&, const double&, const double&) const = 0;
+};
+
+class logit : public dist_family {
+private:
+  static constexpr double THRESH = 30.;
+  static constexpr double MTHRESH = -30.;
+  static constexpr double INVEPS = 1 / DOUBLE_EPS;
+
+public:
+  double link_func(const double &mu) const {
+    return (log(mu / (1 - mu)));
+  }
+
+  double link_func_inv(const double &eta) const{
+    double tmp = (eta < MTHRESH) ? DOUBLE_EPS :
+      ((eta > THRESH) ? INVEPS : exp(eta));
+
+    return( tmp / (1.0 + tmp));
+  };
+
+  double variance(const double &mu) const{
+    return(mu * (1 - mu));
+  };
+
+  double d_mu_d_eta(const double& eta) const{
+    double exp_eta = exp(eta);
+    double opexp = 1 + exp_eta;
+
+    return((eta > THRESH || eta < MTHRESH) ?  DOUBLE_EPS : exp_eta / (opexp * opexp));
+  }
+
+  double dev_resids(const double &y, const double &mu, const double &w) const{
+    return (y > 0.0) ? - log(mu) : - log(1 - mu);
+  };
+};
+
+class poisson : public dist_family {
+public:
+  double link_func(const double &mu) const{
+    return log(mu);
+  }
+
+  double link_func_inv(const double &eta) const{
+    return std::max(exp(eta), DOUBLE_EPS);
+  }
+
+
+  double variance(const double &mu) const{
+    return mu;
+  }
+
+  double d_mu_d_eta(const double &eta) const{
+    return std::max(exp(eta), DOUBLE_EPS);
+  }
+
+  double dev_resids(const double &y, const double &mu, const double &w) const{
+    return (y > 0.0) ? w * (y * log(y / mu) - (y - mu)) : mu * w;
+  }
+};
+
+
+
+
+
+using uword = arma::uword;
+arma::vec IWLS(const arma::mat &X, const arma::vec &y,
+               arma::vec beta, dist_family const * const family,
+               const arma::vec offsets,
+               const uword it_max = 1e3, const double eps = 1.0e-4){
+
+  arma::vec weights(y.n_elem, arma::fill::ones);
+  arma::vec eta = X * beta + offsets;
+  arma::vec mu = eta;
+  mu.for_each([&](arma::mat::value_type &val) { val = family->link_func_inv(val); });
+
+  // TODO: delete
+  // weights.for_each([&](arma::mat::value_type &val) { Rcpp::Rcout << val << "\t"; });
+  // Rcpp::Rcout << std::endl;
+  // eta.for_each([&](arma::mat::value_type &val) { Rcpp::Rcout << val << "\t"; });
+  // Rcpp::Rcout << std::endl;
+  // mu.for_each([&](arma::mat::value_type &val) { Rcpp::Rcout << val << "\t"; });
+  // Rcpp::Rcout << std::endl;
+
+  double deviance_old = 0.0;
+  for(unsigned int i = 0; i < y.size(); i++)
+    deviance_old += family->dev_resids(y[i], mu[i], weights[i]);
+
+  unsigned int it = 0;
+  bool converged = false;
+  while(!converged && it < it_max){
+    arma::vec d_mu_d_eta = eta;
+    d_mu_d_eta.for_each([&](arma::vec::elem_type &val) { val = family->d_mu_d_eta(val); });
+
+    arma::uvec good = arma::find((weights > 0) && (d_mu_d_eta != 0));
+    if(good.n_elem < y.n_elem)
+      Rcpp::warning("Some values 'IWLS' either had zero weights or mu = 0");
+
+    arma::vec variance = mu.elem(good);
+    variance.for_each([&](arma::vec::elem_type &val) { val = family->variance(val); });
+
+    arma::vec z = (eta.elem(good) - offsets.elem(good)) + (y.elem(good) - mu.elem(good)) / d_mu_d_eta.elem(good);
+
+    arma::vec w = arma::sqrt(weights.elem(good) % arma::pow(d_mu_d_eta.elem(good), 2) / variance);
+
+    Cdqrls_res fit = Cdqrls_wrapper(
+      arma::conv_to<arma::mat>::from(X.rows(good)).each_col() % w,
+      z % w, eps, false);
+
+    // TODO: deal with
+    //if(any(fit$coefficients))
+    // ...
+    //if (nobs < fit$rank)
+    // ...
+
+    beta.elem(fit.pivot) = fit.coefficients;
+
+    eta = X * beta + offsets;
+    mu = eta;
+    mu.for_each([&](arma::mat::value_type &val) { val = family->link_func_inv(val); });
+
+    // TODO: delete
+    // Rcpp::Rcout << "boh" << std::endl;
+    // beta.print();
+    //eta.head(10).print();
+
+    double deviance = 0.0;
+    for(unsigned int i = 0; i < y.size(); i++)
+      deviance += family->dev_resids(y[i], mu[i], weights[i]);
+
+    //TODO: Deal with
+    //if (!is.finite(deviance))
+    // ...
+    //if (!(valideta(eta) && validmu(mu)))
+    // ...
+
+    if (std::abs(deviance - deviance_old) / (0.1 + std::abs(deviance)) < eps) {
+      converged = true;
+      break;
+    }
+
+    deviance_old = deviance;
+    ++it;
+  }
+
+  if(!converged)
+    Rcpp::stop("IWLS failed to converge");
+
+  return beta;
+}
+
+
+// The export is only for internal test against glm.fit
+// [[Rcpp::export]]
+arma::vec IWLS_logit(const arma::mat &X, const arma::vec &y,
+                     arma::vec beta,
+                     const arma::vec offsets,
+                     const arma::uword it_max = 1e3, const double eps = 1.0e-4){
+  return(IWLS(X, y, beta, new logit(), offsets, it_max, eps));
+}
+
+// The export is only for internal test against glm.fit
+// [[Rcpp::export]]
+arma::vec IWLS_poisson(const arma::mat &X, const arma::vec &y,
+                       arma::vec beta,
+                       const arma::vec offsets,
+                       const arma::uword it_max = 1e3, const double eps = 1.0e-4){
+  return(IWLS(X, y, beta, new poisson(), offsets, it_max, eps));
+}
---
+++ tests/testthat/testIWLS.R
@@ -0,0 +1,77 @@
+IWLS_logit <- with(environment(ddhazard), IWLS_logit)
+
+# Use the example here
+test_data <- if(grepl("testthat", getwd()))
+  read.csv("binary.csv") else
+    read.csv("tests/testthat/binary.csv")
+
+# GLM fit
+form <- formula(admit ~ .)
+X <- model.matrix(form, data = test_data)
+y <- model.response(model.frame(form, data = test_data))
+beta_0 <- rep(0, ncol(X))
+
+# Compare fits
+for(is_random_offset in c(F, T)){
+  offsets <- if(is_random_offset)
+    rexp(nrow(test_data), 1) else
+      rep(0, nrow(test_data))
+
+  glm_fit <- glm.fit(x = X, y = y, start = beta_0, offset = offsets, family = binomial())
+
+
+  IWLS_fit <-
+    IWLS_logit(X = X, y = y, beta = rep(0, ncol(X)), offsets = offsets, it_max = 1e2,
+               eps = glm.control()$epsilon)
+
+  test_that("glm.fit and IWLS return the same for binomial model",{
+    expect_equal(unname(glm_fit$coefficients), c(IWLS_fit))
+  })
+}
+
+# Do the same for poisson models
+# data from http://www.theanalysisfactor.com/generalized-linear-models-in-r-part-6-poisson-regression-count-variables/
+test_data <-
+  structure(list(Days = c(1L, 2L, 3L, 3L, 4L, 4L, 4L, 6L, 7L, 8L,
+                          8L, 8L, 8L, 12L, 14L, 15L, 17L, 17L, 17L, 18L, 19L, 19L, 20L,
+                          23L, 23L, 23L, 24L, 24L, 25L, 26L, 27L, 28L, 29L, 34L, 36L, 36L,
+                          42L, 42L, 43L, 43L, 44L, 44L, 44L, 44L, 45L, 46L, 48L, 48L, 49L,
+                          49L, 53L, 53L, 53L, 54L, 55L, 56L, 56L, 58L, 60L, 63L, 65L, 67L,
+                          67L, 68L, 71L, 71L, 72L, 72L, 72L, 73L, 74L, 74L, 74L, 75L, 75L,
+                          80L, 81L, 81L, 81L, 81L, 88L, 88L, 90L, 93L, 93L, 94L, 95L, 95L,
+                          95L, 96L, 96L, 97L, 98L, 100L, 101L, 102L, 103L, 104L, 105L,
+                          106L, 107L, 108L, 109L, 110L, 111L, 112L, 113L, 114L, 115L),
+                 Students = c(6L, 8L, 12L, 9L, 3L, 3L, 11L, 5L, 7L, 3L, 8L,
+                              4L, 6L, 8L, 3L, 6L, 3L, 2L, 2L, 6L, 3L, 7L, 7L, 2L, 2L, 8L,
+                              3L, 6L, 5L, 7L, 6L, 4L, 4L, 3L, 3L, 5L, 3L, 3L, 3L, 5L, 3L,
+                              5L, 6L, 3L, 3L, 3L, 3L, 2L, 3L, 1L, 3L, 3L, 5L, 4L, 4L, 3L,
+                              5L, 4L, 3L, 5L, 3L, 4L, 2L, 3L, 3L, 1L, 3L, 2L, 5L, 4L, 3L,
+                              0L, 3L, 3L, 4L, 0L, 3L, 3L, 4L, 0L, 2L, 2L, 1L, 1L, 2L, 0L,
+                              2L, 1L, 1L, 0L, 0L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 1L, 0L, 0L,
+                              0L, 1L, 1L, 0L, 0L, 0L, 0L, 0L)), .Names = c("Days", "Students"
+                              ), class = "data.frame", row.names = c(NA, -109L))
+
+IWLS_poisson <- with(environment(ddhazard), IWLS_poisson)
+
+form <- formula(Students ~ Days)
+X <- model.matrix(form, data = test_data)
+y <- model.response(model.frame(form, data = test_data))
+beta_0 <- rep(0, ncol(X))
+
+# Compare fits
+for(is_random_offset in c(F, T)){
+  offsets <- if(is_random_offset)
+    rexp(nrow(test_data), 1) else
+      rep(0, nrow(test_data))
+
+  glm_fit <- glm.fit(x = X, y = y, start = beta_0, offset = offsets, family = poisson())
+
+
+  IWLS_fit <-
+    IWLS_poisson(X = X, y = y, beta = rep(0, ncol(X)), offsets = offsets, it_max = 1e2,
+               eps = glm.control()$epsilon)
+
+  test_that("glm.fit and IWLS return the same for binomial model",{
+    expect_equal(unname(glm_fit$coefficients), c(IWLS_fit))
+  })
+}
