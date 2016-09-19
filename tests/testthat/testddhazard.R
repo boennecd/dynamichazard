d5eac8b13475965140ddf44aaa5ecb4ae339d0be
boennecd <boennecd@gmail.com>
2016-09-19 23:30:26 +0200

    Added test to ensure that chaning time scale has no effect

--- tests/testthat/testddhazard.R
+++ tests/testthat/testddhazard.R
@@ -141,21 +141,38 @@ test_that("poisson model and logit moels hazzard functions differs", {
 # matplot(fit$a_t_d_s, lty = 2, type = "l", ylim = range(fit$a_t_d_s, sim_dat$betas))
 # matplot(sim_dat$betas, lty = 1, type = "l", add = T)
 
-tmp_file <- file("ukf_log.log")
-sink(tmp_file)
-tryCatch({
-  result_UKF <- ddhazard(
-  formula = Surv(start, stop, event) ~ group,
-  data = head_neck_cancer,
-  by = 1, # Use by month intervals
-  n_max = 10^4, eps = 10^-5,
-  a_0 = rep(0, 2), Q_0 = diag(1, 2), # Initial value
-  est_Q_0 = T,
-  max_T = 45,
-  id = head_neck_cancer$id, order_ = 1,
-  method = "UKF")
-}, finally = {
-  sink()
-  close(tmp_file)
+set.seed(9327)
+sims <- test_sim_func_logit(n_series = 5e2, n_vars = 3, t_0 = 0, t_max = 10,
+                            x_range = 1, x_mean = -.5, re_draw = T, beta_start = 0,
+                            intercept_start = -4, sds = c(.1, rep(1, 3)))
+
+test_that("Chaning time scale in EKF does no change results when other parems are changed accoridngly",{
+  arg_list <- list(formula = survival::Surv(tstart, tstop, event) ~ . - tstart - tstop - event - id,
+                   by = 1,
+                   data = sims$res,
+                   a_0 = rep(0, ncol(sims$res) + 1 - 4),
+                   Q_0 = diag(rep(1e1, ncol(sims$res) + 1 - 4)),
+                   Q = diag(rep(1, ncol(sims$res) + 1 - 4)),
+                   est_Q_0 = F, method = "EKF",
+                   eps = 1e-2, id = sims$res$id,
+                   verbose = F,
+                   max_T = 10)
+
+  res <- do.call(ddhazard, arg_list)
+
+  sim_tmp <- sims
+  t_mult <- 2.5
+  sim_tmp$res$tstart <- sim_tmp$res$tstart * t_mult
+  sim_tmp$res$tstop <- sim_tmp$res$tstop * t_mult
+  arg_list$data <- sim_tmp$res
+  arg_list$by <- t_mult
+  arg_list$max_T <- arg_list$max_T * t_mult
+  arg_list$Q <- arg_list$Q / t_mult
+
+  res_new_time <- do.call(ddhazard, arg_list)
+
+  expect_equal(res$a_t_d_s, res_new_time$a_t_d_s)
+  expect_equal(res$V_t_d_s, res_new_time$V_t_d_s)
+  expect_equal(res$Q, res_new_time$Q * t_mult)
 })
 
