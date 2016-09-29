3a72852a8b5e5d795a83c722143000ac7aa474af
boennecd <boennecd@gmail.com>
2016-09-29 18:15:09 +0200

    Updated the methods for inverse variance and score term for the smoothed lag one correlation matrix in the exponential model

--- tests/testthat/testplot.R
+++ tests/testthat/testplot.R
@@ -7,45 +7,47 @@ result = ddhazard(
   save_risk_set = T, order_ = 1
 )
 
-space_error <- residuals(result, type ="stdSpaceErrors")
+test_that("Update plot tests", expect_true(FALSE))
 
-test_that("Expecting the following calls to succed with a second order fit", {
-  expect_no_error(plot(space_error, result))
-
-  expect_no_error(plot(result, type = "cov", cov_index = 1))
-  expect_no_error(plot(result, type = "cov", cov_index = 2))
-
-  expect_no_error(plot(result, new_data = data.frame(group = 1), method = "delta"))
-  expect_no_error(plot(result, new_data = data.frame(group = 0), method = "delta"))
-  expect_no_error(plot(result, new_data = data.frame(group = 1), method = "backtransform"))
-  expect_no_error(plot(result, new_data = data.frame(group = 0), method = "backtransform"))
-})
-
-# Test second order
-result = ddhazard(
-  formula = Surv(start, stop, event) ~ group,
-  data = head_neck_cancer,
-  by = 1,
-  a_0 = rep(0, 2 * 2), Q_0 = diag(10, 2 * 2),
-  Q = diag(c(1.0e-4, 1.0e-4, 0, 0)), est_Q_0 = F,
-  n_max = 1e3,
-  save_risk_set = T, order_ = 2
-)
-
-test_that("Expecting the following calls to succed with a second order fit", {
-  expect_no_error(plot(result, type = "cov", cov_index = 1))
-  expect_no_error(plot(result, type = "cov", cov_index = 2))
-
-  expect_no_error(plot(result, new_data = data.frame(group = 1), method = "delta"))
-  expect_no_error(plot(result, new_data = data.frame(group = 0), method = "delta"))
-  expect_no_error(plot(result, new_data = data.frame(group = 1), method = "backtransform"))
-  expect_no_error(plot(result, new_data = data.frame(group = 0), method = "backtransform"))
-})
-
-dum <- structure(list(model = "poisson"), "class" = class(result))
-
-test_that("Plot functions throws error when model is poisson",{
-  expect_error(plot(dum))
-})
+# space_error <- residuals(result, type ="stdSpaceErrors")
+#
+# test_that("Expecting the following calls to succed with a second order fit", {
+#   expect_no_error(plot(space_error, result))
+#
+#   expect_no_error(plot(result, type = "cov", cov_index = 1))
+#   expect_no_error(plot(result, type = "cov", cov_index = 2))
+#
+#   expect_no_error(plot(result, new_data = data.frame(group = 1), method = "delta"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 0), method = "delta"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 1), method = "backtransform"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 0), method = "backtransform"))
+# })
+#
+# # Test second order
+# result = ddhazard(
+#   formula = Surv(start, stop, event) ~ group,
+#   data = head_neck_cancer,
+#   by = 1,
+#   a_0 = rep(0, 2 * 2), Q_0 = diag(10, 2 * 2),
+#   Q = diag(c(1.0e-4, 1.0e-4, 0, 0)), est_Q_0 = F,
+#   n_max = 1e3,
+#   save_risk_set = T, order_ = 2
+# )
+#
+# test_that("Expecting the following calls to succed with a second order fit", {
+#   expect_no_error(plot(result, type = "cov", cov_index = 1))
+#   expect_no_error(plot(result, type = "cov", cov_index = 2))
+#
+#   expect_no_error(plot(result, new_data = data.frame(group = 1), method = "delta"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 0), method = "delta"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 1), method = "backtransform"))
+#   expect_no_error(plot(result, new_data = data.frame(group = 0), method = "backtransform"))
+# })
+#
+# dum <- structure(list(model = "poisson"), "class" = class(result))
+#
+# test_that("Plot functions throws error when model is poisson",{
+#   expect_error(plot(dum))
+# })
 
 warning("Implement more plot tests")
