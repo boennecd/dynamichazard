e20b31760d44628984af91f40056557713d07406
boennecd <boennecd@gmail.com>
2016-10-20 12:22:31 +0200

    Intermediate commit. Does not compile

--- tests/testthat/testfixed_terms_estimation.R
+++ tests/testthat/testfixed_terms_estimation.R
@@ -1,2 +1,4 @@
 test_that("Only fixed terms yields the same as bigglm", expect_true(F))
 test_that("Mixtures versus previous fit for both types of models", expect_true(F))
+test_that("Implement UKF", expect_true(F))
+test_that("Implement EKF", expect_true(F))
