3551baffe7fea3da76151144b8eed7e9e6e0d86e
boennecd <boennecd@gmail.com>
2016-12-20 00:39:55 +0100

    Updated ddhazard vignette. Still need to check for spelling errors etc.

--- R/release_questions.R
+++ R/release_questions.R
@@ -1,6 +1,7 @@
 release_questions <- function() {
   c(
     "Have you updated the 'Sim_study_with_logit' vignette?",
-    "Have you pushed to github before running release?"
+    "Have you pushed to github before running release?",
+    "Have you searched the project through for 'TODO'?"
   )
 }
