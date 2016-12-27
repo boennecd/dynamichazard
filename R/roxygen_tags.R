95919b90311c292ee45cd36f7b0401f7dee48122
boennecd <boennecd@gmail.com>
2016-12-27 14:43:39 +0100

    Trying to deal with namespace ntoe on Travis

--- R/roxygen_tags.R
+++ R/roxygen_tags.R
@@ -3,4 +3,5 @@
 #' @importFrom graphics abline axis lines par plot points
 #' @importFrom stats binomial glm model.extract model.matrix poisson predict rexp rnorm runif terms update xtabs
 #' @importFrom utils capture.output tail
+#' @importFrom survival Surv
 NULL
