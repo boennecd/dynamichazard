1ca0d82890b53d1c0248a6937f85aa4f7b6f0708
boennecd <boennecd@gmail.com>
2017-01-05 10:36:00 +0100

    Added weights to UKF implementation

--- tests/testthat/testweights.R
+++ tests/testthat/testweights.R
@@ -46,7 +46,7 @@ test_that("Using weights yields a message about lag_one_cov", {
     regexp = "^lag_one_cov will not be correct when some weights are not 1\n$")
 })
 
-test_that("Making large design mat and using weights yield the same",{
+test_that("Making large design mat and using weights yield the same with EKF",{
   set.seed(9191)
   tmp <- sample.int(nrow(head_neck_cancer), 100, replace = T)
   dum_design <- head_neck_cancer[tmp, ]
@@ -77,6 +77,40 @@ test_that("Making large design mat and using weights yield the same",{
     expect_equal(f1$state_vars, f2$state_vars, info = info, tolerance = 1e-5)
   }
 })
+
+
+test_that("Making large design mat and using weights yield the same with UKF",{
+  set.seed(9191)
+  tmp <- sample.int(nrow(head_neck_cancer), 100, replace = T)
+  dum_design <- head_neck_cancer[tmp, ]
+
+  ws <- sapply(1:nrow(head_neck_cancer), function(x) sum(tmp == x))
+
+  meth <- "UKF"
+  for(m in c("logit", exp_model_names)){
+    f1 <- ddhazard(
+      formula = survival::Surv(stop, event) ~ group,
+      data = dum_design,
+      by = 5, model = m,
+      a_0 = c(-2,0), Q_0 = diag(1, 2), Q = diag(1e-2, 2),
+      control = list(method = meth, ridge_eps = 1e-3),
+      max_T = 25, order = 1)
+
+    suppressMessages(f2 <- ddhazard(
+      formula = survival::Surv(stop, event) ~ group,
+      data = head_neck_cancer,
+      by = 5, model = m,
+      a_0 = c(-2, 0), Q_0 = diag(1, 2), Q = diag(1e-2, 2),
+      control = list(method = meth, ridge_eps = 1e-3),
+      max_T = 25, order = 1,
+      weights = ws))
+
+    info <- paste("m =", m)
+    expect_equal(f1$state_vecs, f2$state_vecs, info = info, tolerance = 1e-5)
+    expect_equal(f1$state_vars, f2$state_vars, info = info, tolerance = 1e-5)
+  }
+})
+
 
 
 
