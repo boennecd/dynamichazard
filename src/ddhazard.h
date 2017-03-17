d89c2c613202b132b3773e944567ba7b71dc3f40
boennecd <boennecd@gmail.com>
2017-03-17 18:16:49 +0100

    Moved UKF to seperate file

--- src/ddhazard.h
+++ src/ddhazard.h
@@ -74,3 +74,151 @@ public:
 
   void solve();
 };
+
+
+
+
+
+// UKF
+class UKF_solver_Org : public Solver{
+  problem_data &p_dat;
+  const arma::uword m;
+  const double k;
+  const double w_0;
+  const double w_i;
+  const double sqrt_m_k;
+  arma::mat sigma_points;
+
+  inline void compute_sigma_points(const arma::vec &a_t,
+                                   arma::mat &s_points,
+                                   const arma::mat &P_x_x){
+    arma::mat cholesky_decomp;
+    if(!arma::chol(cholesky_decomp, P_x_x, "lower")){
+      Rcpp::stop("ddhazard_fit_cpp estimation error: Cholesky decomposition failed");
+    }
+
+    s_points.col(0) = a_t;
+    for(arma::uword i = 1; i < s_points.n_cols; ++i)
+      if(i % 2 == 0)
+        s_points.col(i) = a_t + sqrt_m_k * cholesky_decomp.unsafe_col((i - 1) / 2); else
+          s_points.col(i) = a_t - sqrt_m_k * cholesky_decomp.unsafe_col((i - 1) / 2);
+  }
+
+public:
+  UKF_solver_Org(problem_data &p_, Rcpp::Nullable<Rcpp::NumericVector> &k_);
+
+  void solve();
+};
+
+
+class UKF_solver_New : public Solver{
+protected:
+  problem_data &p_dat;
+  const arma::uword m;
+  const double a;
+  const double k;
+  const double b;
+  const double lambda;
+  const double w_0;
+  const double w_0_c;
+  const double w_0_cc;
+  const double w_i;
+  const double sqrt_m_lambda;
+  arma::mat sigma_points;
+
+  arma::vec weights_vec;
+  arma::vec weights_vec_inv;
+  arma::vec weights_vec_c;
+  arma::vec weights_vec_c_inv;
+  arma::vec weights_vec_cc;
+
+  virtual void Compute_intermediates(const arma::uvec &r_set,
+                                     const arma::vec offsets,
+                                     const int t,
+                                     const double bin_tstart, const double bin_tstop,
+                                     arma::vec &c_vec, arma::mat &O) = 0;
+
+  inline void compute_sigma_points(const arma::vec &a_t,
+                                   arma::mat &s_points,
+                                   const arma::mat &P_x_x){
+    arma::mat cholesky_decomp;
+    if(!arma::chol(cholesky_decomp, P_x_x, "lower")){
+      Rcpp::stop("ddhazard_fit_cpp estimation error: Cholesky decomposition failed");
+    }
+
+    s_points.col(0) = a_t;
+    for(arma::uword i = 1; i < s_points.n_cols; ++i)
+      if(i % 2 == 0)
+        s_points.col(i) = a_t + sqrt_m_lambda * cholesky_decomp.unsafe_col((i - 1) / 2); else
+          s_points.col(i) = a_t - sqrt_m_lambda * cholesky_decomp.unsafe_col((i - 1) / 2);
+  }
+
+public:
+  UKF_solver_New(problem_data &p_, Rcpp::Nullable<Rcpp::NumericVector> &kappa,
+                 Rcpp::Nullable<Rcpp::NumericVector> &alpha,
+                 Rcpp::Nullable<Rcpp::NumericVector> &beta);
+
+  void solve();
+};
+
+
+
+
+class UKF_solver_New_logit : public UKF_solver_New{
+  void Compute_intermediates(const arma::uvec &r_set,
+                             const arma::vec offsets,
+                             const int t,
+                             const double bin_tstart, const double bin_tstop,
+                             arma::vec &c_vec, arma::mat &O);
+
+public:
+  using UKF_solver_New::UKF_solver_New;
+};
+
+
+class UKF_solver_New_exp_bin : public UKF_solver_New{
+  void Compute_intermediates(const arma::uvec &r_set,
+                             const arma::vec offsets,
+                             const int t,
+                             const double bin_tstart, const double bin_tstop,
+                             arma::vec &c_vec, arma::mat &O);
+
+public:
+  using UKF_solver_New::UKF_solver_New;
+};
+
+
+class UKF_solver_New_exp_clip_time : public UKF_solver_New{
+  void Compute_intermediates(const arma::uvec &r_set,
+                             const arma::vec offsets,
+                             const int t,
+                             const double bin_tstart, const double bin_tstop,
+                             arma::vec &c_vec, arma::mat &O);
+
+public:
+  using UKF_solver_New::UKF_solver_New;
+};
+
+
+class UKF_solver_New_exp_clip_time_w_jump : public UKF_solver_New{
+  void Compute_intermediates(const arma::uvec &r_set,
+                             const arma::vec offsets,
+                             const int t,
+                             const double bin_tstart, const double bin_tstop,
+                             arma::vec &c_vec, arma::mat &O);
+
+public:
+  using UKF_solver_New::UKF_solver_New;
+};
+
+
+class UKF_solver_New_exponential : public UKF_solver_New{
+  void Compute_intermediates(const arma::uvec &r_set,
+                             const arma::vec offsets,
+                             const int t,
+                             const double bin_tstart, const double bin_tstop,
+                             arma::vec &c_vec, arma::mat &O);
+
+public:
+  using UKF_solver_New::UKF_solver_New;
+};
